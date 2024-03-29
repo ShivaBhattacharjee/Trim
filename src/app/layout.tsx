b8df897de8457920890755efcffa299c967cfc28
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2024-03-30 03:00:13 +0530

    nvm dumb css mistake

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,24 +9,22 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Trim-Shorten Your Links",
-  description: "Shorten your links with ease using Trim",
+    title: "Trim-Shorten Your Links",
+    description: "Shorten your links with ease using Trim",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${inter.className} relative overflow-hidden bg-black text-white min-h-screen max-w-[1200px] m-auto`}
-      >
-        <Toaster />
-        <Navbar />
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${inter.className} relative  bg-black text-white min-h-screen max-w-[1200px] m-auto`}>
+                <Toaster />
+                <Navbar />
+                {children}
+            </body>
+        </html>
+    );
 }
